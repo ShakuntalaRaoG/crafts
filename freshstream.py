a17fcd5dc26ec29b521b8c89fb7ae964daddf4ed
gsr <sha>
2024-08-25 08:34:45 -0400

    new images

---
+++ freshstream.py
@@ -0,0 +1,43 @@
+import streamlit as st
+
+st.title("🎈 My Crafts")
+st.write(
+    "Welcome to My Crafts Page."
+)
+st.image('OmGaneshayaNamaha.jpeg')
+st.write(
+    "Sri.Ganesha"
+)
+st.image('Vivekananda.jpeg')
+st.write(
+    "Vivekanandaphoto_Chicago"
+)
+st.image('Penholder.jpeg')
+st.write(
+    "Penholder"
+)
+st.image('backcover.jpeg')
+st.write(
+    "backcover"
+)
+st.image('decor.jpeg.jpeg')
+st.write(
+    "decor.jpeg"
+)
+st.image('Handnitmuffler.jpeg')
+st.write(
+    "Handknit Muffler"
+)
+st.image('small_boots.jpeg')
+st.write(
+    "Tiny boots"
+)
+st.image('Coaster.jpeg')
+st.write(
+    "Coaster"
+)
+st.image('Tablespread.jpeg')
+st.write
+("Tablespread")
+
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,75 @@ import streamlit as st
 
 st.title("🎈 My Crafts")
 st.write(
-    "Welcome to My Crafts Page."
+    "Welcome to My Crafts Pages."
+)
+st.image('OmGaneshayaNamaha.jpeg')
+st.write(
+    "Sri.Ganesha"
+)
+st.image('Vivekananda.jpeg')
+st.write(
+    "Vivekanandaphoto_Chicago"
+)
+st.image('Penholder.jpeg')
+st.write(
+    "Penholder"
+)
+st.image('backcover.jpeg')
+st.write(
+    "backcover"
+)
+st.image('decor.jpeg')
+st.write(
+    "decor.jpeg"
 )
 st.image('small_boots.jpeg')
-st.image('backcover.jpeg')
+st.write(
+    "Tiny boots"
+)
+st.image('Coaster.jpeg')
+st.write(
+    "Coaster"
+)
+st.image('Trovewithflowers.jpeg')
+st.write("Trovewithflowers")
+st.image('NewDeco.jpeg')
+st.write("New Decorative crochet")
+st.image('AllCoasters.jpeg')
+st.write("AllCoasters")
+st.image('Sampleflowers.jpeg')
+st.write("Sampleflowers")
+st.image('Cap.jpeg')
+st.write("Cap")
+st.image('verticalflowers.jpeg')
+st.write("Papernapkin's stand Flowers")
+st.image('Sweater_part1.jpeg')
+st.write("Sweater_part1")
+st.image('Sweater_part2.jpeg')
+st.write("Sweater_part2")
+st.image('Sweater_1.jpeg')
+st.write("Mrs.Dillon Taught")
+st.image('Sweater_2.jpeg')
+st.write("Mrs.Vijaya Kokil_Indore")
+st.image('Sweater_3.jpeg')
+st.write("Courtesy Youtube")
+st.image('Sweater_4.jpeg')
+st.write("Courtesy Youtube")
+st.image ('Tablespread.jpeg')
+st.write("Tablespread")
+st.image ('With_leftoverwool.jpeg')
+st.write("With_leftoverwool")
+st.image ('Suarecrochet3.jpeg')
+st.write("Suarecrochet3")
+st.image('Handnitmuffler.jpeg')
+st.write(
+    "Handknit Muffler"
+)
+st.image('Chaircover.jpeg')
+st.write(
+    "Chaircover"
+)
+
+
+ 
+         
