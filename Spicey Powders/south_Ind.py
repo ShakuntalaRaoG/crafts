485f4aa4fcec17350e9ad2bbbbe4e5583b6c2d6b
gsr <sha>
2024-08-26 17:26:27 -0400

    new subject

---
+++ Spicey Powders/south_Ind.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.title("Useful Spicey Powders")
+ 
+st.markdown(''':rainbow[Welcome to my useful spicey powders]:balloon::sparkles::bouquet::rainbow::stars::moon:''')
+
+
+
+
+
+ 
