d3fe04fbc964473ae52b6897beb73d575b759021
gsr <sha>
2024-08-26 17:33:43 -0400

    added an item

--- Spicey Powders/south_Ind.py
+++ Spicey Powders/south_Ind.py
@@ -2,9 +2,5 @@ import streamlit as st
 st.title("Useful Spicey Powders")
  
 st.markdown(''':rainbow[Welcome to my useful spicey powders]:balloon::sparkles::bouquet::rainbow::stars::moon:''')
-
-
-
-
-
- 
+st.image('lady_spiceypowders.jpeg')
+st.write("Chutney Powder:-")
