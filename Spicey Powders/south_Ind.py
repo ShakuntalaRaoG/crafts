3c1e206b74119c57496dd0064e75c58c0786959b
gsr <sha>
2024-08-26 18:29:50 -0400

    lady spice

--- Spicey Powders/south_Ind.py
+++ Spicey Powders/south_Ind.py
@@ -1,6 +1,14 @@
 import streamlit as st
-st.title("Useful Spicey Powders")
+st.markdown(''':rainbow[Welcome to My Spicey Powders]:balloon::sparkles::bouquet::rainbow::stars::moon:''')
+
+st.image('lady_spiceypowders.jpeg', caption='lady_spiceypowders')
+
+
  
-st.markdown(''':rainbow[Welcome to my useful spicey powders]:balloon::sparkles::bouquet::rainbow::stars::moon:''')
-st.image('lady_spiceypowders.jpeg')
-st.write("Chutney Powder:-")
+
+
+
+
+  
+
+ 
