d0766a9098e869c1fcb23ab35a06c20784873d08
gsr <sha>
2024-08-26 21:16:38 -0400

    Chatnipudi

--- Spicey Powders/south_Ind.py
+++ Spicey Powders/south_Ind.py
@@ -2,9 +2,11 @@ import streamlit as st
 st.markdown(''':rainbow[Welcome to My Spicey Powders]:balloon::sparkles::bouquet::rainbow::stars::moon:''')
 
 st.image('lady_spiceypowders.jpeg', caption='lady_spiceypowders')
-
-
+st.text_input('ChutniPowder.txt')
  
+
+
+
 
 
 
