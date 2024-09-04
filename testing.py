5f39e3e6896d850e9e49b3d8812ef36859a4ce45
gsr <sha>
2024-09-04 11:51:57 -0400

    stnew

---
+++ testing.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("🎈 My Testing Crafts")
+st.write(
+    "Welcome to My Crafts Pages."
+)
+st.image('oldestworkingProfessor.jpeg')
+st.write("crafts/Oldest working Professor")
