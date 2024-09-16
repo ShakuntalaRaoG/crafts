732206a92118c7934805f40ad29f6bdd7353ed7f
gsr <sha>
2024-09-15 22:47:08 -0400

    cccc

--- streamlit_app.py
+++ streamlit_app.py
@@ -111,7 +111,7 @@ st.image('Verticallyjoined squares.jpeg')
 st.write("Vertically joined squares") 
 
 st.image('rosesandleaves_matee cloth.jpeg')
-st.write("Roses andleaves on matee cloth")
+st.write("Roses and leaves on matee cloth")
 
 
  
